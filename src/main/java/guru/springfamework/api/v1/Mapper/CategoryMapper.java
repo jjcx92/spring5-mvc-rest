0565cba000a4107d8c9d137758f77188b8da7d56
jjcx92 <jjcx92@gmail.com>
2020-07-01 10:02:06 -0600

    category mapper final.

--- src/main/java/guru/springfamework/api/v1/Mapper/CategoryMapper.java
+++ src/main/java/guru/springfamework/api/v1/Mapper/CategoryMapper.java
@@ -1,14 +1,18 @@
 package guru.springfamework.api.v1.Mapper;
 
 import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 /**
  * Juerghens castro on 07-01-20 and  08:55 AM to 2020
  */
 @Mapper
-public class CategoryMapper {
+public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
-    CategoryDTO categoryToCategoryDTO(category category);
+    //Mapping (source = "get_id", target="Id")
+    CategoryDTO categoryToCategoryDTO(Category category);
 }
