d8c2bef09d308766f6259d6b3e2d6d90ae039367
jjcx92 <jjcx92@gmail.com>
2020-07-09 18:49:14 -0600

    adding restcontroller

--- src/main/java/guru/springfamework/controllers/v1/CategoryController.java
+++ src/main/java/guru/springfamework/controllers/v1/CategoryController.java
@@ -6,37 +6,52 @@ import guru.springfamework.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Juerghens castro on 07-02-20 and  05:45 AM to 2020
  */
-@Controller
+//@Controller
+@ResponseBody
 @RequestMapping(CategoryController.BASE_URL)
 public class CategoryController {
 
-    public static final String BASE_URL="/api/v1/categories/";
+    public static final String BASE_URL = "/api/v1/categories/";
     private final CategoryService categoryService;
 
-    public CategoryController(CategoryService categoryService)
-    {
+    public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
-    @GetMapping()
+
+
+    //oldway
+    /*
+        @GetMapping()
     public ResponseEntity<CategoryListDTO>getAllCategories() {
 
         return new ResponseEntity<CategoryListDTO>(
                 new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    }*/
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDTO getAllCategories() {
+
+        return new CategoryListDTO(categoryService.getAllCategories());
     }
 
-    @GetMapping("{name}")
+
+    /*@GetMapping("{name}")
     public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
 
         return new ResponseEntity<CategoryDTO>
                 (categoryService.getCategoryByName(name),HttpStatus.OK);
+    }*/
+    @GetMapping("{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name) {
+
+        return categoryService.getCategoryByName(name);
     }
 
 }
--- src/main/java/guru/springfamework/controllers/v1/CustomerController.java
+++ src/main/java/guru/springfamework/controllers/v1/CustomerController.java
@@ -11,13 +11,14 @@ import org.springframework.web.bind.annotation.*;
 /**
  * Juerghens castro on 07-02-20 and  09:23 AM to 2020
  */
-@Controller
+//@Controller
+@RestController
 //@RequestMapping("/api/v1/customers")
 @RequestMapping(CustomerController.BASE_URL)
 public class CustomerController {
 
 
-    public static final String BASE_URL="/api/v1/customers";
+    public static final String BASE_URL = "/api/v1/customers";
     private final CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {
@@ -25,7 +26,7 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping
+  /*  @GetMapping
     public ResponseEntity<CustomerListDTO> getAllCustomers() {
 
         return new ResponseEntity<CustomerListDTO>
@@ -46,25 +47,64 @@ public class CustomerController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<CustomerDTO>updateCustomer(@PathVariable Long id,@RequestBody CustomerDTO customerDTO) {
-        return new ResponseEntity<CustomerDTO>(customerService.saveCustomerByDTO(id,customerDTO)
-        ,HttpStatus.OK);
+    public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
+        return new ResponseEntity<CustomerDTO>(customerService.saveCustomerByDTO(id, customerDTO)
+                , HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<CustomerDTO>patchCustomer(@PathVariable Long id,@RequestBody CustomerDTO customerDTO) {
-        return new ResponseEntity<CustomerDTO>(customerService.patchCustomer(id,customerDTO)
-                ,HttpStatus.OK);
+    public ResponseEntity<CustomerDTO> patchCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
+        return new ResponseEntity<CustomerDTO>(customerService.patchCustomer(id, customerDTO)
+                , HttpStatus.OK);
     }
 
     @DeleteMapping({"/{id}"})
-    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id){
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
 
-         customerService.deleteCustomerById(id);
+        customerService.deleteCustomerById(id);
 
         if (id <= 0 || id == null) {
             return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
         }
-        return new  ResponseEntity<Void>(HttpStatus.OK);
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }*/
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public CustomerListDTO getListOfCustomers(){
+        return new CustomerListDTO(customerService.getAllCustomer());
     }
+
+    @GetMapping({"/{id}"})
+    @ResponseStatus(HttpStatus.OK)
+    public CustomerDTO getCustomerById(@PathVariable Long id){
+        return customerService.getCustomerById(id);
+    }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CustomerDTO createNewCustomer(@RequestBody CustomerDTO customerDTO){
+        return customerService.createNewCustomer(customerDTO);
+    }
+
+    @PutMapping({"/{id}"})
+    @ResponseStatus(HttpStatus.OK)
+    public CustomerDTO updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO){
+        return customerService.saveCustomerByDTO(id, customerDTO);
+    }
+
+    @PatchMapping({"/{id}"})
+    @ResponseStatus(HttpStatus.OK)
+    public CustomerDTO patchCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO){
+        return customerService.patchCustomer(id, customerDTO);
+    }
+
+    @DeleteMapping({"/{id}"})
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCustomer(@PathVariable Long id){
+        customerService.deleteCustomerById(id);
+    }
+
+
 }
